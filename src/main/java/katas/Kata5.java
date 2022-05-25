0ef2a0ce7a0c247c8ac512ade8d7ba0feb86aabb
jpabloallin <jpac.647@gmail.com>
2022-05-25 16:11:52 -0500

    - Solved Kata5 exercise.

--- src/main/java/katas/Kata5.java
+++ src/main/java/katas/Kata5.java
@@ -19,6 +19,6 @@ public class Kata5 {
     public static Double execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return 3.0;
+        return movies.stream().map(Movie::getRating).reduce(Double.MIN_VALUE, Double::max);
     }
 }
